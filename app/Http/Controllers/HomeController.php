91dead52720a556c2c0bed87f87881cd82eb8bef
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-12-05 19:40:18 +0000

    Displaying Users data in the categories component

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('welcome');
+        $users = User::all();
+
+        return view('welcome', [
+            'users'=>$users
+        ]);
     }
 }
